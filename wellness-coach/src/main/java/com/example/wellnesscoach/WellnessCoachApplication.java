5c15b77a66f4cf45160d3ee1fb0553622eae6cc0
Yeonjae37 <jyj030307@gmail.com>
2024-07-16 18:45:47 +0900

    chore: initial Spring project setup

---
+++ wellness-coach/src/main/java/com/example/wellnesscoach/WellnessCoachApplication.java
@@ -0,0 +1,13 @@
+package com.example.wellnesscoach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WellnessCoachApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WellnessCoachApplication.class, args);
+    }
+
+}

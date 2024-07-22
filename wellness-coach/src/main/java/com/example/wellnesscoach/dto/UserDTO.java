68c90ed4560d7b5eb96640448816f3417d1ff056
Yeonjae37 <jyj030307@gmail.com>
2024-07-22 17:53:56 +0900

    feat: add User Entity

---
+++ wellness-coach/src/main/java/com/example/wellnesscoach/dto/UserDTO.java
@@ -0,0 +1,13 @@
+package com.example.wellnesscoach.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDTO {
+
+    private String role;
+    private String name;
+    private String username;
+}
---
+++ wellness-coach/src/main/java/com/example/wellnesscoach/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.wellnesscoach.repository;
+
+import com.example.wellnesscoach.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    UserEntity findByUsername(String username);
+}

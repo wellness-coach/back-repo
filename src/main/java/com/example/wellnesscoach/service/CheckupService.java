6fb999e800dfa2bc880d6e386f63085217448d7e
yeonjae037 <jyj030307@gmail.com>
2024-07-24 04:15:27 +0900

    feat: add CheckupService

---
+++ src/main/java/com/example/wellnesscoach/service/CheckupService.java
@@ -0,0 +1,23 @@
+package com.example.wellnesscoach.service;
+
+import com.example.wellnesscoach.entity.Checkup;
+import com.example.wellnesscoach.repository.CheckupRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CheckupService {
+
+    private CheckupRepository checkupRepository;
+
+    public CheckupService(CheckupRepository checkupRepository) {
+        this.checkupRepository = checkupRepository;
+    }
+
+    public Checkup save(Checkup checkup) {
+
+    }
+
+    public Checkup submit(Checkup checkup) {
+
+    }
+}

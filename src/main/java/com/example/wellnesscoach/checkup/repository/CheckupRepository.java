ac197f9f249a9b352cda376ac178b22bde83c621
yeonjae037 <jyj030307@gmail.com>
2024-07-25 05:12:54 +0900

    feat: add Checkup

--- src/main/java/com/example/wellnesscoach/checkup/repository/CheckupRepository.java
+++ src/main/java/com/example/wellnesscoach/checkup/repository/CheckupRepository.java
@@ -1,6 +1,6 @@
-package com.example.wellnesscoach.repository;
+package com.example.wellnesscoach.checkup.repository;
 
-import com.example.wellnesscoach.entity.Checkup;
+import com.example.wellnesscoach.checkup.Checkup;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CheckupRepository extends JpaRepository<Checkup, Long> {

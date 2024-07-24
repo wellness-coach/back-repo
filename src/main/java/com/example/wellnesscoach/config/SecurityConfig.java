66f97cd14d51f23c519398faa18ad6a8a8613408
yeonjae037 <jyj030307@gmail.com>
2024-07-24 20:45:55 +0900

    fix: 이전 설정 취소

--- src/main/java/com/example/wellnesscoach/config/SecurityConfig.java
+++ src/main/java/com/example/wellnesscoach/config/SecurityConfig.java
@@ -7,7 +7,6 @@ import com.example.wellnesscoach.service.CustomOAuth2UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -26,13 +25,11 @@ public class SecurityConfig {
     private final CustomOAuth2UserService customOAuth2UserService;
     private final CustomSuccessHandler customSuccessHandler;
     private final JWTUtil jwtUtil;
-    private final Environment env;
 
-    public SecurityConfig(CustomOAuth2UserService customOAuth2UserService, CustomSuccessHandler customSuccessHandler, JWTUtil jwtUtil, Environment env) {
+    public SecurityConfig(CustomOAuth2UserService customOAuth2UserService, CustomSuccessHandler customSuccessHandler, JWTUtil jwtUtil) {
         this.customOAuth2UserService = customOAuth2UserService;
         this.customSuccessHandler = customSuccessHandler;
         this.jwtUtil = jwtUtil;
-        this.env = env;
     }
 
     @Bean
@@ -40,45 +37,59 @@ public class SecurityConfig {
 
         http
                 .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+
                     @Override
                     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
                         CorsConfiguration configuration = new CorsConfiguration();
+
                         configuration.setAllowedOrigins(Arrays.asList("https://localhost:3000", "http://localhost:8080", "http://localhost:3000",
                                 "https://www.wellnesscoach.store", "http://localhost:5173"));
                         configuration.setAllowedMethods(Collections.singletonList("*"));
                         configuration.setAllowCredentials(true);
                         configuration.setAllowedHeaders(Collections.singletonList("*"));
                         configuration.setMaxAge(3600L);
+
                         configuration.setExposedHeaders(Arrays.asList("Set-Cookie", "Authorization"));
+
                         return configuration;
                     }
                 }));
 
-        // CSRF 비활성화
-        http.csrf(csrf -> csrf.disable());
 
-        // Form 로그인 비활성화
-        http.formLogin(formLogin -> formLogin.disable());
+        //csrf disable
+        http
+                .csrf((auth) -> auth.disable());
 
-        // HTTP Basic 인증 비활성화
-        http.httpBasic(httpBasic -> httpBasic.disable());
+        //From 로그인 방식 disable
+        http
+                .formLogin((auth) -> auth.disable());
 
-        // JWT 필터 추가
-        http.addFilterAfter(new JWTFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
+        //HTTP Basic 인증 방식 disable
+        http
+                .httpBasic((auth) -> auth.disable());
 
-        // OAuth2 로그인 설정
-        http.oauth2Login(oauth2 -> oauth2
-                .userInfoEndpoint(userInfoEndpointConfig -> userInfoEndpointConfig.userService(customOAuth2UserService))
-                .successHandler(customSuccessHandler));
+        //JWTFilter 추가
+        http
+                .addFilterAfter(new JWTFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
 
-        // 경로별 인가 설정
-        http.authorizeHttpRequests(auth -> auth
-                .requestMatchers("/", "/favicon.ico", "/main", "/custom-login", "/oauth2/authorization/**").permitAll()
-                .anyRequest().authenticated());
+        //oauth2
+        http
+                .oauth2Login((oauth2 -> oauth2
+                        .userInfoEndpoint(userInfoEndpointConfig -> userInfoEndpointConfig
+                                .userService(customOAuth2UserService))
+                        .successHandler(customSuccessHandler)));
 
-        // 세션 관리 설정: STATELESS
-        http.sessionManagement(session -> session
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        //경로별 인가 작업
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/").permitAll()
+                        .anyRequest().authenticated());
+
+        //세션 설정 : STATELESS
+        http
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
     }

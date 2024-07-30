88fa036c0a58c04430942a410ac1e56b74e864ad
Yeonjae37 <jyj030307@gmail.com>
2024-07-30 23:44:30 +0900

    feat: result 추가 및 스코어 저장 로직 구현

---
+++ src/main/java/com/example/wellnesscoach/domain/result/Result.java
@@ -0,0 +1,35 @@
+package com.example.wellnesscoach.domain.result;
+
+import com.example.wellnesscoach.domain.checkup.Checkup;
+import com.example.wellnesscoach.domain.meal.MenuType;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Result {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long resultId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "checkup_id")
+    private Checkup checkup;
+
+    @Enumerated(EnumType.STRING)
+    private MenuType menuType;
+
+    private Integer score;
+
+
+    public void saveResult(
+            Checkup checkup,
+            MenuType menuType,
+            Integer score
+    ){
+        this.checkup = checkup;
+        this.menuType = menuType;
+        this.score = score;
+    }
+}

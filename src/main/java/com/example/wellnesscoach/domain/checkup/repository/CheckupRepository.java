70031663f254a7225d5b9017f1b152b4b679e278
Yeonjae37 <jyj030307@gmail.com>
2024-07-31 21:23:48 +0900

    feat: CheckupRepository findByDate 추가

--- src/main/java/com/example/wellnesscoach/domain/checkup/repository/CheckupRepository.java
+++ src/main/java/com/example/wellnesscoach/domain/checkup/repository/CheckupRepository.java
@@ -1,7 +1,15 @@
 package com.example.wellnesscoach.domain.checkup.repository;
 
 import com.example.wellnesscoach.domain.checkup.Checkup;
+import com.example.wellnesscoach.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+
 public interface CheckupRepository extends JpaRepository<Checkup, Long> {
+    Checkup findByDate(LocalDate date);
+
+    List<Checkup> findAllCheckupsByUser(User user);
 }
